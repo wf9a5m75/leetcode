f7761af06deba014e984e1ebd437e266a2b231bc
masashi <wf9a5m75@gmail.com>
2021-10-20 11:15:33 -0700

    Add the editorial solution (using heap sort)

--- top-k-frequent-elements/solution.py
+++ top-k-frequent-elements/solution.py
@@ -1,6 +1,17 @@
 from collections import Counter
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        # O(N)
+        counts = Counter(nums)
+
+        # heap sort
+        # O(N log k) times
+        return heapq.nlargest(k, counts.keys(), key=counts.get)
+
+
+    def topKFrequent_normal(self, nums: List[int], k: int) -> List[int]:
         results = Counter(nums).most_common(k)
         results = list(map(lambda x: x[0], results))
         return results
