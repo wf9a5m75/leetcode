ff1fb82c54621048b07b43fb90434886a9064661
masashi <wf9a5m75@gmail.com>
2021-11-04 16:21:57 -0700

    update: 283. Move Zeroes

--- move-zeroes/solution.py
+++ move-zeroes/solution.py
@@ -1,5 +1,18 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
+        n = len(nums)
+        lastNonZero = 0
+        lastZero = 0
+        while(lastNonZero < n):
+            while(lastNonZero < n) and (nums[lastNonZero] == 0):
+                lastNonZero += 1
+            if (lastNonZero < n):
+                nums[lastZero], nums[lastNonZero] = nums[lastNonZero], nums[lastZero]
+                lastZero += 1
+                lastNonZero += 1
+
+
+    def moveZeroes_acceptable_but_slow(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
