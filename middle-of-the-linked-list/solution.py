5bb2ea8af615718481f862ac5f1baa9b7811dde3
masashi <wf9a5m75@gmail.com>
2021-11-04 19:37:47 -0700

    solve: 876. Middle of the Linked List

---
+++ middle-of-the-linked-list/solution.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        p1 = p2 = head
+        while(p2) and (p2.next):
+            p1 = p1.next
+            p2 = p2.next
+            if (p2):
+                p2 = p2.next
+        return p1
