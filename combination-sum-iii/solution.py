eb59f1df2b2c1b56be71a9336291773c850a943e
masashi <wf9a5m75@gmail.com>
2021-11-19 12:25:33 -0800

    Resolved: 216. Combination Sum III

--- combination-sum-iii/solution.py
+++ combination-sum-iii/solution.py
@@ -1,5 +1,22 @@
 class Solution:
-    def combinationSum3(self, k: int, n: int, path: List[int] = [], mem = set()) -> List[List[int]]:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans = []
+
+        def backtracking(path, remain, curr):
+            if len(path) == k:
+                if remain == 0:
+                    ans.append(path.copy())
+                return
+            for i in range(curr + 1, 10):
+                if remain - i >= 0:
+                    path.append(i)
+                    backtracking(path, remain - i, i)
+                    path.pop()
+        backtracking([], n, 0)
+
+        return ans
+    
+    def combinationSum3_slow(self, k: int, n: int, path: List[int] = [], mem = set()) -> List[List[int]]:
         if k >= 10:
             return []
 
