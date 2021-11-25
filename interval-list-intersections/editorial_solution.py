02d5377af748ead4df2a40ddf24473ef01b88e81
masashi <wf9a5m75@gmail.com>
2021-11-25 07:57:15 -0800

    Rewrite the editorial solution

--- interval-list-intersections/editorial_solution.py
+++ interval-list-intersections/editorial_solution.py
@@ -1,10 +1,12 @@
 class Solution:
-    def intervalIntersection(self, A, B):
-        ans, i, j = [], 0, 0
-        while i < len(A) and j < len(B):
-            if (A[i][0] <= B[j][1]) and (A[i][1] >= B[j][0]):
+    def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        ans = []
+        nA, nB = len(A), len(B)
+        i = j = 0
+        while(i < nA) and (j < nB):
+            if (A[i][0] <= B[j][1]) and (B[j][0] <= A[i][1]):
                 ans.append([max(A[i][0], B[j][0]), min(A[i][1], B[j][1])])
-            if (A[i][1] < B[j][1]):
+            if A[i][1] < B[j][1]:
                 i += 1
             else:
                 j += 1
