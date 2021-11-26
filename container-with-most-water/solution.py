a95c799544df62c1189b449fd83758f446de24a8
masashi <wf9a5m75@gmail.com>
2021-11-26 13:06:11 -0800

    Update: Container With Most Water

--- container-with-most-water/solution.py
+++ container-with-most-water/solution.py
@@ -1,46 +1,31 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         L, R = 0, len(height) - 1
+
+        # To determine the maximum value at each range,
+        # find the highest value before moving.
+        highest = max(height)
+
+        # answer
         maxWater = 0
         while(L != R):
+
+            # Calculate the containable water
             water = min(height[L], height[R]) * (R - L)
+            # print(L, height[L:R+1], R, " = ", water)
+
+            # Compair with the maximum so far.
             maxWater = max(maxWater, water)
+
+            # If the maxWater is grater than
+            # the maximum containable water at this range,
+            # it means there is no greater containable water range than current.
+            if (maxWater >= highest * (R - L)):
+                break
+
+            # Move pointer of lower side
             if (height[L] < height[R]):
                 L += 1
             else:
                 R -= 1
         return maxWater
-
-
-#     def maxArea2(self, height: List[int]) -> int:
-#         N = len(height)
-#         dpL = [0] * N
-#         dpR = [0] * N
-
-#         prevL = 0
-#         prevR = N - 1
-#         for i in range(N):
-#             if height[prevL] < height[i]:
-#                 dpL[i] = i
-#                 prevL = i
-#             else:
-#                 dpL[i] = prevL
-
-#             if height[prevR] < height[N - 1 - i]:
-#                 dpR[N - 1 - i] = N - 1 - i
-#                 prevR = N - 1 - i
-#             else:
-#                 dpR[N - 1 - i] = prevR
-
-
-#         # print(dpL)
-#         # print(dpR)
-#         ans = 0
-#         for i in range(N):
-#             ans = max(ans, min(height[dpL[i]], height[dpR[i]]) * (dpR[i] - dpL[i]))
-
-#             ans = max(ans, min(height[i], height[N - 1]) * (N - 1 - i))
-
-#             ans = max(ans, min(height[i], height[0]) * i)
-
-#         return ans
