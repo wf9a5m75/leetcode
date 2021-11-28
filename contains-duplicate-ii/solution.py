75d51a85a555c4572d3698dcce4d10ac60d932f4
masashi <wf9a5m75@gmail.com>
2021-11-27 19:04:15 -0800

    Add another solution: 219. Contains Duplicate II

--- contains-duplicate-ii/solution.py
+++ contains-duplicate-ii/solution.py
@@ -1,8 +1,30 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        #
+        # hash approach
+        #   time complexity: O(N)
+        #   space complexity: O(N)
+        
         mem = {}
         for i, val in enumerate(nums):
             if (val in mem) and (i - mem[val] <= k):
                 return True
             mem[val] = i
+        return False
+
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        #
+        # sliding window approach
+        #   time complexity: O(N)
+        #   space complexity: O(k)
+
+        N = len(nums)
+        mem = set()
+        for i in range(N):
+            if i - k - 1 >= 0:
+                mem.remove(nums[i - k - 1])
+
+            if nums[i] in mem:
+                return True
+            mem.add(nums[i])
         return False
