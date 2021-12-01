d3fafd7376db8e446849f4f4f8553f2809e33d54
masashi <wf9a5m75@gmail.com>
2021-11-30 19:57:56 -0800

    Add comments

--- all-paths-from-source-to-target/solution.py
+++ all-paths-from-source-to-target/solution.py
@@ -7,13 +7,19 @@ class Solution:
             # add the current index to the path
             path.append(currIdx)
 
+            # Stop traversing if we visit `n-1`
             if len(graph) -1 == currIdx:
-                results.append(path)
+                results.append(path.copy())
+                path.pop()
                 return
 
+            # Move to the next nodes
             currNode = graph[currIdx]
             for toIdx in currNode:
-                walker(toIdx, path.copy())
+                walker(toIdx, path)
 
+            path.pop()
+
+        # We approach with backtracking
         walker(0, [])
         return results
