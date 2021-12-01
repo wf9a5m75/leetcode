955d2d117957c99e0c27858adc35bf1361295ce8
masashi <wf9a5m75@gmail.com>
2021-11-30 22:03:01 -0800

    Add comments

--- subsets/solution.py
+++ subsets/solution.py
@@ -1,8 +1,8 @@
-import heapq
-from typing import List
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        #
+        # (1) Iteration approach
+        #
 
         """
         [] + [1] => [1]
@@ -19,8 +19,38 @@ class Solution:
             []
         ]
         for num in nums:
+
+            # Generate new combination using previous result
             for i in range(len(results)):
                 # print(results[i], "+", "[{}]".format(num), "=>", results[i] + [num])
                 results.append(results[i] + [num])
             # print("----")
         return results
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        #
+        # (2) Backtracking approach
+        #
+        outputs = []
+
+        # backtracking
+        def backtracking(k, n, first = 0, curr = []):
+            # if the combination is done
+            if (len(curr) == k):
+                outputs.append(curr.copy())
+                return
+
+            for i in range(first, n):
+                # add nums[i] into the current combination
+                curr.append(nums[i])
+
+                # use next integers to complete the combination
+                backtracking(k, n, i + 1, curr)
+
+                curr.pop()
+
+
+        n = len(nums)
+        for k in range(n + 1):
+            backtracking(k, n)
+        return outputs
