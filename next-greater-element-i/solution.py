a13b04fc34031ed2f12cfb338721d388f11ef224
masashi <wf9a5m75@gmail.com>
2021-12-02 16:20:28 -0800

    update: Next Greater Element I

--- next-greater-element-i/solution.py
+++ next-greater-element-i/solution.py
@@ -1,6 +1,9 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        stack = deque()
+        #
+        # Reverse approach
+        #
+        stack = []
         mem = {}
         for num2 in reversed(nums2):
 
@@ -18,6 +21,35 @@ class Solution:
 
             stack.append(num2)
 
+        # Build the answer
+        result = []
+        for num1 in nums1:
+            result.append(mem[num1])
+
+        return result
+
+
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        #
+        # Forward approach
+        #
+        stack = []
+        mem = {}
+        for num2 in nums2:
+
+            # The smaller numbers than num2 are appeared before.
+            # It means there is no greater number the current num2.
+            while(stack) and (num2 > stack[-1]):
+                mem[stack.pop()] = num2
+
+            # There is a possibility that
+            # there is no value, the greater number than the current num2,
+            # so we put num2 = -1
+            mem[num2] = -1
+
+            stack.append(num2)
+
+        # Build the answer
         result = []
         for num1 in nums1:
             result.append(mem[num1])
