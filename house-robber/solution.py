4fe5ba2fcfe8fe6d40f33fcbb3da9ff8550d6b1c
masashi <wf9a5m75@gmail.com>
2021-12-27 14:20:38 -0800

    Update: 198. House Robber

--- house-robber/solution.py
+++ house-robber/solution.py
@@ -1,23 +1,24 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if (len(nums) == 1):
-            return nums[0]
-        if (len(nums) == 2):
-            return max(nums[0], nums[1])
 
         N = len(nums)
-        dp = [0] * N
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        for i in range(2, N):
-            # Pick the higher total amount from two cases:
-            #  1) robbing the house at i - 2
-            #          -> we can rob the house at i
-            #
-            #  2) or robbing the house i - 1
-            #          -> we can Not rob the house at i,
-            #             because the house i - 1 has security system
-            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        if (N == 1):
+            return nums[0]
+        if (N == 2):
+            return max(nums[0], nums[1])
 
+        dp = [0] * N
+
+        # Assuming we rob at index 0
+        dp[0] = nums[0]
+
+        # If we choose dp[0], it means we start from 0, then skip at index 1.
+        # If we choose nums[1], it means we start from 1 (we didn't rob at index 0)
+        dp[1] = max(dp[0], nums[1])
+
+        for i in range(2, N):
+            # In order to rob at index i, we must be located at i-2 before.
+            # Otherwise, we skip the house at index i.
+            # Records the maximum amount.
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
         return dp[N - 1]
-        
