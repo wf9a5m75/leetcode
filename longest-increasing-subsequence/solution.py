d892a094904dce0acf0bb293ceecd74db6b768a3
masashi <wf9a5m75@gmail.com>
2021-12-31 15:29:25 -0800

    300. Longest Increasing Subsequence

--- longest-increasing-subsequence/solution.py
+++ longest-increasing-subsequence/solution.py
@@ -1,14 +1,13 @@
 class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        heights.append(0)
-        stack = [-1]
-        N = len(heights)
-        maxArea = 0
-        for i in range(N):
-            while(stack) and (heights[i] < heights[stack[-1]]):
-                h = heights[stack.pop()]
-                w = i - stack[-1] - 1
-                area = h * w
-                maxArea = max(maxArea, area)
-            stack.append(i)
-        return maxArea
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        size = len(nums)
+        # dp[i] denotes the maximum Length Of Subseq possible for characters from index {i} if i included
+        dp = [1]*size  # LIS
+
+        for i in range(size-2, -1, -1):
+            # we can take one by one combination with sub-indexes
+            for j in range(i+1, size):
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], 1 + dp[j])
+
+        return max(dp)
