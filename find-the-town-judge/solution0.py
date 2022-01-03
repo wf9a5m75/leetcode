a81b505e982b8464e4d19146b3115b825ce053ed
masashi <wf9a5m75@gmail.com>
2022-01-02 22:20:24 -0800

    Add another solution: 997. Find the Town Judge

--- find-the-town-judge/solution0.py
+++ find-the-town-judge/solution0.py
@@ -1,5 +1,8 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
+
+        # Since everyone except the judge trust one person,
+        # the judge should have n-1 score.
         people = [0 for _ in range(n)]
         for trustGraph in trust:
             people[trustGraph[0] - 1] -= 1
---
+++ find-the-town-judge/solution1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        graph = [-1 for i in range(n + 1)]
+        scores = [0] * (n + 1)
+
+        for peopleA, peopleB in trust:
+
+            # Find the root
+            path = []
+            while(graph[peopleB] == peopleB):
+                path.append(peopleB)
+                peopleB = graph[peopleB]
+
+            graph[peopleA] = peopleB
+
+            # Update the nodes on the path
+            for p in path:
+                graph[p] = peopleB
+
+            # Increasing the score
+            scores[peopleB] += 1
+
+        # Find the town judge
+        #   The town judge does not trust nobody (graph = -1)
+        #   and everyone trust the judge (score = n - 1)
+        for i in range(1, n + 1):
+            if (graph[i] == -1) and (scores[i] == n - 1):
+                return i
+        return -1
